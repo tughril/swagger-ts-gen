8e98d294705488e9eeef40b06ffc73c5185fd5a9
sue71 <s.masaki07@gmail.com>
2020-06-11 09:16:45 +0900

    feat: add barrel

--- src/generator.ts
+++ src/generator.ts
@@ -6,9 +6,7 @@ import {
   Path,
   Spec,
   Response,
-  Parameter,
-  PathParameter,
-  BodyParameter
+  Parameter
 } from "swagger-schema-official";
 import {
   isBodyParameter,
@@ -67,6 +65,8 @@ export class Generator {
     const operationTmpl =
       templates.operation ||
       fs.readFileSync(path.resolve(__dirname, "../templates/operation.hbs"), "utf-8");
+    const barrelTmpl = 
+      fs.readFileSync(path.resolve(__dirname, "../templates/barrel.hbs"), "utf-8");
 
     // Register partial for handlebars
     this.registerPartial();
@@ -92,7 +92,17 @@ export class Generator {
         Handlebars.compile(definitionTmpl),
         definitionDir
       ),
-      ...this.createOperations(data.operations, Handlebars.compile(operationTmpl), operationDir)
+      this.createDefinitionBarrel(
+        data.definitions,
+        Handlebars.compile(barrelTmpl),
+        definitionDir
+      ),
+      ...this.createOperations(data.operations, Handlebars.compile(operationTmpl), operationDir),
+      this.createOperationBarrel(
+        data.operations,
+        Handlebars.compile(barrelTmpl),
+        operationDir
+      ),
     ]);
   }
 
@@ -159,7 +169,7 @@ export class Generator {
     // Parse response
     if (responses) {
       const response = responses["200"] || responses["201"];
-      if (response.schema) {
+      if (response && response.schema) {
         return mapTS(response.schema);
       }
     }
@@ -172,7 +182,7 @@ export class Generator {
    */
   private parsePathParameter(parameters: Parameter[]): TSSchema {
     // Parse path parameters
-    return (parameters || []).filter(isPathParameter).reduce((res, v: PathParameter) => {
+    return (parameters || []).filter(isPathParameter).reduce((res, v) => {
       res.properties[v.name] = mapTS(v, v.required);
       return res;
     }, emptySchema());
@@ -185,7 +195,7 @@ export class Generator {
   private parseQueryParameter(parameters: Parameter[]): TSSchema {
     // Parse query parameters
     return (parameters || [])
-      .filter(v => isQueryParameter(v))
+      .filter(isQueryParameter)
       .reduce((res: TSSchema, v) => {
         res.properties[v.name] = mapTS(v, v.required);
         return res;
@@ -199,8 +209,8 @@ export class Generator {
   private parseBodyParameter(parameters: Parameter[]): TSSchema {
     // Parse body parameters
     return (parameters || [])
-      .filter(v => isBodyParameter(v))
-      .map((v: BodyParameter) => mapTS(v.schema!, v.required))[0];
+      .filter(isBodyParameter)
+      .map((v) => mapTS(v.schema!, v.required))[0];
   }
 
   /**
@@ -233,6 +243,38 @@ export class Generator {
         content: template(v)
       };
     });
+  }
+
+  /**
+   * Create operations request
+   * @param operations
+   * @param template
+   */
+  private createOperationBarrel(
+    operations: OperationSchema[],
+    template: Handlebars.TemplateDelegate,
+    directory: string
+  ): GenCodeRequest {
+    return {
+      filepath: path.resolve(directory, "index.ts"),
+      content: template(operations)
+    };
+  }
+
+  /**
+   * Create operations request
+   * @param operations
+   * @param template
+   */
+  private createDefinitionBarrel(
+    definitions: DefinitionSchema[],
+    template: Handlebars.TemplateDelegate,
+    directory: string
+  ): GenCodeRequest {
+    return {
+      filepath: path.resolve(directory, "index.ts"),
+      content: template(definitions)
+    };
   }
 
   /**
